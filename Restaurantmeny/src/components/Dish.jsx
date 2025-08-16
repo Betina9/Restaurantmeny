cfdcb6941b01d5b5ca343e98c0f2fb53c395adc3
Betina9 <ob042025@gmail.com>
2025-08-17 01:51:20 +0200

    dish tar imot en rett

--- Restaurantmeny/src/components/Dish.jsx
+++ Restaurantmeny/src/components/Dish.jsx
@@ -1,1 +1,15 @@
 import styles from "../styles/Dish.module.css";
+
+function Dish({ data }) {
+  const { tittelen, pris, ingredienser, kateogri } = data;
+  return (
+    <article className={styles.dish}>
+      <header className="styles.header">
+        <h3>{tittel}</h3>
+        <span className={styles.price}>{pris}</span>
+      </header>
+      <p className="styles.ingredients">{ingredienser}</p>
+      <small className={styles.category}>{kategori}</small>
+    </article>
+  );
+}
